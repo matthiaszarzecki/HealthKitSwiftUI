f4c5822b75a90899615a3a3995aff8f0214cd41a
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-05-04 11:16:12 +0200

    Create project

---
+++ HealthKitApp/HealthKitApp/HealthKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitAppApp.swift
+//  HealthKitApp
+//
+//  Created by Matthias Zarzecki on 04.05.21.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitAppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
