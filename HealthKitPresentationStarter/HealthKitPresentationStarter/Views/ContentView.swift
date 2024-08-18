804b02358578162a63b49b124d4b3aaae5abd15c
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2024-08-18 10:45:34 +0200

    Add shells for new presentation

--- HealthKitPresentationStarter/HealthKitPresentationStarter/Views/ContentView.swift
+++ HealthKitPresentationStarter/HealthKitPresentationStarter/Views/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+  @ObservedObject private var allStepsViewModel = AllStepsViewModel()
+
   var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+      VStack {
+        Image(systemName: "heart.fill")
+          .imageScale(.large)
+          .foregroundStyle(Color.red)
+
+
+        Text("StepData: \(allStepsViewModel.steps)")
+
+        Button(
+          action: allStepsViewModel.loadData,
+          label: {
+            Text("Get Health Data")
+          }
+        )
+      }
+      .padding()
     }
     .padding()
+  }
+}
+
+struct Step: Identifiable {
+  let id = UUID()
+  let count: Int
+  let date: Date
+}
+
+class AllStepsViewModel: ObservableObject {
+  @Published private(set) var steps: [Step] = []
+
+  func loadData() {
+
   }
 }
 
