a4a758ca6d5044c5409eacbdb96866d768eec789
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2024-08-18 08:22:17 +0200

    Add empty presentation project

---
+++ HealthKitPresentation/HealthKitPresentation/AppDelegate/HealthKitPresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitPresentationApp.swift
+//  HealthKitPresentation
+//
+//  Created by Matthias Zarzecki on 18.08.24.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitPresentationApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
