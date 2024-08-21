516e440e15610a233b2c02f2bb54c6f05fe82f94
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2024-08-21 10:09:54 +0200

    Add commented out code to starter

--- HealthKitPresentationStarter/HealthKitPresentationStarter/Views/ContentView.swift
+++ HealthKitPresentationStarter/HealthKitPresentationStarter/Views/ContentView.swift
@@ -20,7 +20,9 @@ struct ContentView: View {
         Text("StepData: \(steps)")
 
         Button(
-          action: loadData,
+          action: {
+
+          },
           label: {
             Text("Get Health Data")
           }
@@ -29,10 +31,6 @@ struct ContentView: View {
       .padding()
     }
     .padding()
-  }
-
-  private func loadData() {
-
   }
 }
 
@@ -45,3 +43,138 @@ struct Step: Identifiable {
 #Preview {
   ContentView()
 }
+
+/*
+ func loadData() {
+   // Request permission from User. A standard iOS
+   // HealthKit Permission Overlay will appear.
+   healthStore.requestAuthorization { success in
+
+     // Permission Granted.
+     if success {
+
+       // Get actual data we want to display.
+       self.healthStore.calculateSteps { statisticsCollection in
+         if let statisticsCollection = statisticsCollection {
+
+           // Reset steps here for later fresh appending
+           self.steps = []
+
+           self.updateUIFromStatistics(
+             statisticsCollection: statisticsCollection
+           )
+         }
+       }
+     }
+   }
+ }
+
+ private func updateUIFromStatistics(
+   statisticsCollection: HKStatisticsCollection
+ ) {
+   // Get dates we want to display. The display should
+   // begin 6 days ago (0 indexed for 1 week total),
+   // and end on the current day.
+   let startDate = Calendar.current.date(
+     byAdding: .day,
+     value: -6, to: Date()
+   )!
+   let endDate = Date()
+
+   statisticsCollection.enumerateStatistics(
+     from: startDate,
+     to: endDate
+   ) { statistics, stop in
+     let count = statistics.sumQuantity()?.doubleValue(for: .count())
+     let step = Step(
+       count: Int(count ?? 0),
+       date: statistics.startDate
+     )
+     self.steps.append(step)
+   }
+ }
+ */
+
+/*
+
+ class HealthStore {
+   var healthStore: HKHealthStore?
+
+   init() {
+     if HKHealthStore.isHealthDataAvailable() {
+       healthStore = HKHealthStore()
+     }
+   }
+
+   func requestAuthorization(
+     completion: @escaping (Bool) -> Void
+   ) {
+     guard let healthStore = healthStore else {
+       return completion(false)
+     }
+
+     guard let stepType = HKQuantityType.quantityType(
+       forIdentifier: HKQuantityTypeIdentifier.stepCount
+     ) else {
+       return completion(false)
+     }
+
+     healthStore.requestAuthorization(
+       toShare: [],
+       read: [stepType]
+     ) { success, error in
+       completion(success)
+     }
+   }
+
+   func calculateSteps(
+     completion: @escaping (HKStatisticsCollection?) -> Void
+   ) {
+     guard let stepType = HKQuantityType.quantityType(
+       forIdentifier: HKQuantityTypeIdentifier.stepCount
+     ) else {
+       completion(nil)
+       return
+     }
+
+     let startDate = Calendar.current.date(
+       byAdding: .day,
+       value: -6,
+       to: Date()
+     )
+     let anchorDate = Calendar(identifier: .iso8601).date(
+       from: Calendar(
+         identifier: .iso8601
+       ).dateComponents(
+         [.yearForWeekOfYear, .weekOfYear],
+         from: Date()
+       )
+     )!
+     let daily = DateComponents(day: 1)
+
+     let predicate = HKQuery.predicateForSamples(
+       withStart: startDate,
+       end: Date(),
+       options: .strictStartDate
+     )
+
+     let query = HKStatisticsCollectionQuery(
+       quantityType: stepType,
+       quantitySamplePredicate: predicate,
+       options: .cumulativeSum,
+       anchorDate: anchorDate,
+       intervalComponents: daily
+     )
+
+     query.initialResultsHandler = { query, statisticsCollection, error in
+       completion(statisticsCollection)
+     }
+
+     if let healthStore = healthStore {
+       healthStore.execute(query)
+     }
+   }
+
+
+ }
+ */
